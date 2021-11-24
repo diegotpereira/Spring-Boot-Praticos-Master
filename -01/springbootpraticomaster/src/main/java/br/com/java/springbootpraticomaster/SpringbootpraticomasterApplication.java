7a4691654379ead97da20c8908e27ef3e1a65ea2
diego <diegoteixeirapereira@hotmail.com>
2021-11-24 11:28:29 -0300

    first commit

---
+++ -01/springbootpraticomaster/src/main/java/br/com/java/springbootpraticomaster/SpringbootpraticomasterApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.springbootpraticomaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpraticomasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpraticomasterApplication.class, args);
+	}
+
+}
---
+++ -01/springbootpraticomaster/src/test/java/br/com/java/springbootpraticomaster/SpringbootpraticomasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.springbootpraticomaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpraticomasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
