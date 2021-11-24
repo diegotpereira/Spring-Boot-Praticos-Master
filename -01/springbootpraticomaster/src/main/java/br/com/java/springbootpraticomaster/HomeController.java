8de684aae957e7ee3f46188d2f278d7a585c9d3a
diego <diegoteixeirapereira@hotmail.com>
2021-11-24 11:36:41 -0300

    injetando dados através do model.

--- -01/springbootpraticomaster/src/main/java/br/com/java/springbootpraticomaster/HomeController.java
+++ -01/springbootpraticomaster/src/main/java/br/com/java/springbootpraticomaster/HomeController.java
@@ -1,13 +1,15 @@
 package br.com.java.springbootpraticomaster;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     
     @RequestMapping("/")
-    public String homeApp() {
+    public String homeApp(Model model) {
+        model.addAttribute("mensagem", "esta foi uma mensagem injetada atraves do model");
 
         return "index";
     }
