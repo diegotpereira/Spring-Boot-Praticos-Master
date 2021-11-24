ae29cb967ca0f734c6b732b0f7b046a0f024dfb5
diego <diegoteixeirapereira@hotmail.com>
2021-11-24 14:41:27 -0300

    criação da aplicação spring boot security.

--- -03 - Spring Security/springbootpraticosmaster/src/main/java/br/com/java/springbootpraticosmaster/SecurityConfiguration.java
+++ -03 - Spring Security/springbootpraticosmaster/src/main/java/br/com/java/springbootpraticosmaster/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.com.java.springbootpraticosmaster;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
     }
 
     @Override
